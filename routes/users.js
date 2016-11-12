47ad05bb3ff4ee790a1b561510c71b2ffce6d981
Bharatt Kukreja <bkukreja@uwaterloo.ca>
2016-11-12 16:09:45 -0500

    Login functionality added

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user')
 
 /* GET users listing. */
@@ -92,6 +93,45 @@ router.post('/register', function(req, res, next) {
     res.redirect('/');
   }
 
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  User.getUserByUsername(username, function(err, user) {
+    if(err) throw err;
+    if(!user) {
+      console.log('Unknown user');
+      return done(null, false, {message: 'Unknown User'})
+    }
+
+    User.comparePassword(password, user.password, function(err, isMatch) {
+      if(err) throw err;
+      if(isMatch) {
+        return done(null, user);
+      } else {
+        console.log('Password is: ' + password);
+        console.log('Actual Password is: ' + user.password);
+        console.log('Invalid Password');
+        return done(null, false, {message: 'Invalid Password'});
+      }
+    });
+
+  });
+}));
+
+router.post('/login', passport.authenticate('local', {successRedirect: '/', failureRedirect:'/users/login', failureFlash:'Invalid username or password'}), function(req, res) {
+  console.log('Authentication Successful');
+  req.flash('success', 'You are logged in');
+  res.redirect('/');
 });
 
 module.exports = router;
